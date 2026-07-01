0f99d35cf2c8091e749e524c5963cb56b0f7a8a0
Skywalker93-hub <nothern4lights@gmail.com>
2026-07-01 16:46:25 +0300

    Refactor log processing script to improve IP extraction and file writing logic

--- processing-web-server-logs.py
+++ processing-web-server-logs.py
@@ -1,29 +1,33 @@
+import collections
+import os
+
 #Variables 
-old_log_file = "nginx.log"
-new_log_file = "result.nginx.log"
+old_log_file = "test.test.log"
+new_log_file = "testresult.nginx.log"
 
 #The function as a generator extracts IP addresses without storing the temporary variable with this data in RAM and saving memory
-def extract_ip(old_log_file):
-    with open(old_log_file, 'r', encoding='utf-8') as f:
-        for line in f:
-            yield line.split()[0] 
+if os.path.exists(old_log_file):
+    def extract_ip(old_log_file):
+        with open(old_log_file, 'r', encoding='utf-8') as f:
+            for line in f:
+                if line.strip():
+                    yield line.split()[0]
+else:
+    print(f"File {old_log_file} does not exist.")
+    exit(1)
 
 #The function extracts the data from the dictionary and writes the data in the needed format to a file 
-def write_to_file(new_log_file, ip_list):
+def write_to_file(new_log_file, sorted_ip_list):
     with open(new_log_file, 'w+', encoding='utf-8') as f:
-        for x0, x1 in ip_list.items():
-           output_line = f"{x0}: {x1}\n"
+        for ip, count in sorted_ip_list.items():
+           output_line = f"{ip}: {count}\n"
            f.write(output_line)
             
-#The function creates the dictionary that stores IP addressess and their number of request
+# The function creates the dictionary that stores IP addressess and their number of request
 def count_ip(group_ip):
-    ip_list = {}
-    for ip in group_ip:
-        if ip in ip_list:
-            ip_list[ip] += 1
-        else:
-            ip_list[ip] = 1
-    write_to_file(new_log_file, ip_list)
+    ip_list = collections.Counter(group_ip)
+    sorted_ip_list = dict(sorted(ip_list.items(), key=lambda item: item[1], reverse=True))
+    write_to_file(new_log_file, sorted_ip_list)
 
 group_ip = extract_ip(old_log_file)
 count_ip(group_ip)
