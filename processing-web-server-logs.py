992247101917dfb21b3c33d011746fab2ec3fcfd
Skywalker93-hub <nothern4lights@gmail.com>
2026-07-01 16:52:56 +0300

    Refactor IP extraction logic to separate function and improve error handling

--- processing-web-server-logs.py
+++ processing-web-server-logs.py
@@ -6,15 +6,11 @@ old_log_file = "test.test.log"
 new_log_file = "testresult.nginx.log"
 
 #The function as a generator extracts IP addresses without storing the temporary variable with this data in RAM and saving memory
-if os.path.exists(old_log_file):
-    def extract_ip(old_log_file):
-        with open(old_log_file, 'r', encoding='utf-8') as f:
-            for line in f:
-                if line.strip():
-                    yield line.split()[0]
-else:
-    print(f"File {old_log_file} does not exist.")
-    exit(1)
+def extract_ip(old_log_file):
+    with open(old_log_file, 'r', encoding='utf-8') as f:
+        for line in f:
+            if line.strip():
+                yield line.split()[0]
 
 #The function extracts the data from the dictionary and writes the data in the needed format to a file 
 def write_to_file(new_log_file, sorted_ip_list):
@@ -29,5 +25,11 @@ def count_ip(group_ip):
     sorted_ip_list = dict(sorted(ip_list.items(), key=lambda item: item[1], reverse=True))
     write_to_file(new_log_file, sorted_ip_list)
 
-group_ip = extract_ip(old_log_file)
+# Call the function to extract IP addresses from the log file and count their occurrences
+if os.path.exists(old_log_file):
+    group_ip = extract_ip(old_log_file)
+else:
+    print(f"File {old_log_file} does not exist.")
+    exit(1)
+
 count_ip(group_ip)
